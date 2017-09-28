ee2afd42a2efa726ff2db1d721b0826cb4c372aa
tomliuauto <tomliuauto@gmail.com>
2017-09-27 22:21:51 -0500

    add locations controller 4.2

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-// var homepageController = function (req, res) {
-// res.render('index', { title: 'Express' });
-// };
-
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-router.get('/', ctrlMain.index);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
