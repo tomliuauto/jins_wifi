eb4df8689aa183b1715f00b1284a6b473d9fb7d5
Tom Liu <tomliuauto@gmail.com>
2017-09-29 05:22:31 +0000

    add the detail 100

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ res.render('locations-list', { title: 'Home' });
 };
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-res.render('index', { title: 'Location info' });
+res.render('location-info', { title: 'Location info' });
 };
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
