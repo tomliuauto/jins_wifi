b7396a556d909badb539262b8a5d8f86910ce6bc
tomliuauto <tomliuauto@gmail.com>
2017-09-30 19:52:14 -0500

    Added the view templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,5 +8,5 @@ res.render('location-info', { title: 'Location info' });
 };
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-res.render('index', { title: 'Add review' });
+res.render('location-review-form', { title: 'Add review' });
 };
