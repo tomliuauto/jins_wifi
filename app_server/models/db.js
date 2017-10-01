c9c6aa13f193a41c56f7aaf96c7211c030f09d42
tomliuauto <tomliuauto@gmail.com>
2017-10-01 16:55:43 -0500

    connected to mongoDB

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,15 @@
 var mongoose = require( 'mongoose' );
+
+var dbURI = 'mongodb://localhost/jinswifi';
+
+// mongoose.connect(dbURI);
+
+// mongoose.createConnection(uri, { server: { poolSize: 4 }});
+
+mongoose.createConnection(dbURI);
+
+// mongoose.connect(dbURI, {
+//   socketTimeoutMS: 0,
+//   keepAlive: true,
+//   reconnectTries: 30
+// });
